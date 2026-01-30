4fed581800d77890baa501287543aafc7716bff4
= <=>
2026-01-30 09:13:33 +0000

    first commit

---
+++ backend/src/main/java/com/hsbc/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.hsbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
